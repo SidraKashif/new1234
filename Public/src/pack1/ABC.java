2c8ba09b4b034781a8bac3cd1e61ee69bcc53ae0
SidraKashif <108953451+SidraKashif@users.noreply.github.com>
2023-01-17 16:43:38 -0600

    guygygyguy

---
+++ Public/src/pack1/ABC.java
@@ -0,0 +1,12 @@
+package pack1;
+
+public class ABC {
+	
+	public int data=10;
+	public void m1() 
+	{
+		System.out.println(data);
+		
+	}
+
+}
---
+++ Public/src/pack2/XYZ.java
@@ -0,0 +1,15 @@
+package pack2;
+
+import pack1.ABC;
+
+public class XYZ {
+
+	public static void main(String[] args) {
+		ABC abc=new ABC();
+		{
+			abc.m1();
+		}
+
+	}
+
+}
